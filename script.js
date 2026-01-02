de0b288bac6a7201d62291fa1aedcbf9a7d3cab0
Oscar Otero <oom@oscarotero.com>
2026-01-02 16:58:16 +0100

    load data and images from jsdelivr

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 import vento from "https://cdn.jsdelivr.net/npm/ventojs@2.3.0/web.js";
 
+const baseUrl = new URL("https://cdn.jsdelivr.net/gh/oscarotero/visual-tabs/");
+
 const env = vento({
-  includes: new URL(import.meta.resolve("./templates")),
+  includes: new URL("templates/", baseUrl),
 });
 
-const { slides } = await (await fetch("./data.json")).json();
+env.filters.fullUrl = (path) => new URL("." + path, baseUrl).href;
+
+const { slides } = await (await fetch(new URL("data.json", baseUrl))).json();
 const index = Math.floor(Math.random() * slides.length);
 const slide = slides[index];
 const template = `${slide.type}.vto`;
